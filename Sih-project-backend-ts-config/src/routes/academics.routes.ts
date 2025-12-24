d228f0991e3f5e32a99c50b1b4976b2ca453f6e7
Bhavya Khatri <133779496+bhavya-shikamaru@users.noreply.github.com>
2025-12-24 18:50:50 +0530

    Delete Sih-project-backend-ts-config directory

--- Sih-project-backend-ts-config/src/routes/academics.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createAcademic } from "../controllers/academics.controller";
-
-const router = Router();
-
-router.post("/", createAcademic);
-
-export default router;
--- Sih-project-backend-ts-config/src/routes/attendance.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createAttendance } from "../controllers/attendance.controller";
-
-const router = Router();
-
-router.post("/", createAttendance);
-
-export default router;
